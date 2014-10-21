b0c837c0ef6713ce6290e8f8a1f8444d81683592
reobin <rg1530@gmail.com>
2014-10-21 14:34:11 -0400

    Modified Controller

--- src/controller/BoardController.java
+++ src/controller/BoardController.java
@@ -5,6 +5,10 @@ import view.MenuView;
 import model.BoardModel;
 import model.TokenModel.State;
 
+/**
+ * Classe BoardController qui utilise les modèles et gère les événements envoyés par les vues
+ * GameView et MenuView.
+ */
 public class BoardController 
 {
 	private GameView gameView;
@@ -14,6 +18,10 @@ public class BoardController
 	private int[] settings;
 	private int nbTokens;
 	
+	/**
+	 * Constructeur du BoardController. Initialise les vues GameView et MenuView, reçoit les
+	 * settings et initialise le tableau de jeu.
+	 */
 	public BoardController()
 	{
 		this.gameView = new GameView(this);
@@ -26,6 +34,12 @@ public class BoardController
 		this.gameView.initBoard(this.settings[0], this.settings[1]);
 	}
 	
+	/**
+	 * Classe permettant d'ajouter logiquement un jeton (changer l'état de celui-ci) dans 
+	 * le tableau de jeu.
+	 * @param columnIndex La colonne où le jeton doit être ajouté
+	 * @return La rangée à laquelle le jeton a été ajouté
+	 */
 	public int addToken(int columnIndex)
 	{
 		int x = findColumnHeight(columnIndex);
@@ -33,6 +47,11 @@ public class BoardController
 		return x;
 	}
 	
+	/**
+	 * Trouve à quelle rangée un jeton doit être ajouter selon la colonne
+	 * @param columnIndex La colonne à vérifier
+	 * @return La rangée où le jeton doit être ajouté
+	 */
 	public int findColumnHeight(int columnIndex)
 	{
 		for (int i = this.settings[0] - 1; i >= 0; i--)
@@ -45,6 +64,9 @@ public class BoardController
 		return -1;
 	}
 	
+	/**
+	 * Remet le tableau de jeu à zéro.
+	 */
 	public void reset()
 	{
 		this.currentPlayer = State.RED;
@@ -52,6 +74,9 @@ public class BoardController
 		boardModel = new BoardModel(this.settings[0], this.settings[1]);
 	}
 	
+	/**
+	 * Permet d'alterner entre les joueurs
+	 */
 	public void changePlayer()
 	{
 		if (this.currentPlayer == State.RED)
@@ -64,7 +89,29 @@ public class BoardController
 		}
 	}
 	
+	/**
+	 * Détermine si la partie est terminée ou non
+	 * @param x La position en X du dernier jeton placé
+	 * @param yLa position en Y du dernier jeton placé
+	 * @return Un booléen, true si la partie est terminée, false si elle ne l'est pas.
+	 */
 	public boolean isGameFinished(int x, int y)
+	{
+		return this.LookLeftToRight(x, y) || 
+			   this.LookLeftToRightDiagonal(x, y) || 
+			   this.LookRightToLeftDiagonal(x, y) ||
+			   this.LookUpToDown(x, y);
+
+	}
+	
+	
+	/**
+	 * Vérifie si la partie a été gagné en diagonale de gauche à droite
+	 * @param x La position du jeton en X
+	 * @param y La position du jeton en Y
+	 * @return Un booléen, true si la partie a été gagné, false si non.
+	 */
+	private boolean LookLeftToRightDiagonal(int x, int y)
 	{
 		this.nbTokens = 0;
 		
@@ -76,6 +123,17 @@ public class BoardController
 			return true;
 		}
 		
+		return false;
+	}
+	
+	/**
+	 * Vérifie si la partie a été gagné en diagonale de droite à gauche
+	 * @param x La position du jeton en X
+	 * @param y La position du jeton en Y
+	 * @return Un booléen, true si la partie a été gagné, false si non.
+	 */
+	private boolean LookRightToLeftDiagonal(int x, int y)
+	{
 		this.nbTokens = 0;
 
 		this.LookUpperRight(x, y);
@@ -86,6 +144,17 @@ public class BoardController
 			return true;
 		}
 		
+		return false;
+	}
+	
+	/**
+	 * Vérifie si la partie a été gagné en ligne droite horizontale
+	 * @param x La position du jeton en X
+	 * @param y La position du jeton en Y
+	 * @return Un booléen, true si la partie a été gagné, false si non.
+	 */
+	private boolean LookLeftToRight(int x, int y)
+	{
 		this.nbTokens = 0;
 		
 		this.LookRight(x, y);
@@ -96,6 +165,17 @@ public class BoardController
 			return true;
 		}
 		
+		return false;
+	}
+	
+	/**
+	 * Vérifie si la partie a été gagné en ligne droite verticale
+	 * @param x La position du jeton en X
+	 * @param y La position du jeton en Y
+	 * @return Un booléen, true si la partie a été gagné, false si non.
+	 */
+	private boolean LookUpToDown(int x, int y)
+	{
 		this.nbTokens = 0;
 		
 		this.LookDown(x, y);
@@ -108,11 +188,21 @@ public class BoardController
 		return false;
 	}
 	
+	
+	/**
+	 * Retourne le modèle du tableau
+	 * @return Le modèle du tableau
+	 */
 	public BoardModel getBoardModel()
 	{
 		return this.boardModel;
 	}
 	
+	/**
+	 * Vérifie si une colonne est pleine
+	 * @param nbColumn La colonne à vérifier
+	 * @return Un booléen, true si la colonne est pleine, false si elle ne l'est pas
+	 */
 	public boolean IsColumnFull(int nbColumn)
 	{
 		if (this.boardModel.getBoard()[0][nbColumn].getState() != State.EMPTY)
@@ -123,6 +213,10 @@ public class BoardController
 		return false;
 	}
 	
+	/**
+	 * Vérifie si le tableau de jeu est plein, signifiant une partie nulle
+	 * @return Un booléen, true si le tableau est plein, false s'il ne l'est pas.
+	 */
 	public boolean IsBoardFull()
 	{
 		int nbFullColumns = 0;
@@ -136,7 +230,12 @@ public class BoardController
 		return nbFullColumns == this.settings[1];
 	}
 	
-	public void LookUpperLeft(int positionX, int positionY)
+	/**
+	 * Vérifie en haut à gauche du jeton à la position spécifiée.
+	 * @param positionX La position du jeton en X
+	 * @param positionY La position du jeton en Y
+	 */
+	private void LookUpperLeft(int positionX, int positionY)
 	{
 		try
 		{
@@ -148,11 +247,16 @@ public class BoardController
 		}
 		catch(ArrayIndexOutOfBoundsException e)
 		{
-			
+			 e.printStackTrace();
 		}
 	}
 	
-	public void LookUpperRight(int positionX, int positionY)
+	/**
+	 * Vérifie en haut à droite du jeton à la position spécifiée.
+	 * @param positionX La position du jeton en X
+	 * @param positionY La position du jeton en Y
+	 */
+	private void LookUpperRight(int positionX, int positionY)
 	{
 		try
 		{
@@ -164,11 +268,16 @@ public class BoardController
 		}
 		catch(ArrayIndexOutOfBoundsException e)
 		{
-			
+			e.printStackTrace();
 		}
 	}
 	
-	public void LookLowerLeft(int positionX, int positionY)
+	/**
+	 * Vérifie en bat à gauche du jeton à la position spécifiée.
+	 * @param positionX La position du jeton en X
+	 * @param positionY La position du jeton en Y
+	 */
+	private void LookLowerLeft(int positionX, int positionY)
 	{
 		try
 		{
@@ -180,11 +289,16 @@ public class BoardController
 		}
 		catch(ArrayIndexOutOfBoundsException e)
 		{
-			
+			e.printStackTrace();
 		}
 	}
 	
-	public void LookLowerRight(int positionX, int positionY)
+	/**
+	 * Vérifie en bas à droite du jeton à la position spécifiée.
+	 * @param positionX La position du jeton en X
+	 * @param positionY La position du jeton en Y
+	 */
+	private void LookLowerRight(int positionX, int positionY)
 	{
 		try
 		{
@@ -196,11 +310,16 @@ public class BoardController
 		}
 		catch(ArrayIndexOutOfBoundsException e)
 		{
-			
+			e.printStackTrace();
 		}
 	}
 	
-	public void LookDown(int positionX, int positionY)
+	/**
+	 * Vérifie en bas du jeton à la position spécifiée.
+	 * @param positionX La position du jeton en X
+	 * @param positionY La position du jeton en Y
+	 */
+	private void LookDown(int positionX, int positionY)
 	{
 		try
 		{
@@ -212,11 +331,16 @@ public class BoardController
 		}
 		catch(ArrayIndexOutOfBoundsException e)
 		{
-			
+			e.printStackTrace();
 		}
 	}
 	
-	public void LookLeft(int positionX, int positionY)
+	/**
+	 * Vérifie à gauche du jeton à la position spécifiée.
+	 * @param positionX La position du jeton en X
+	 * @param positionY La position du jeton en Y
+	 */
+	private void LookLeft(int positionX, int positionY)
 	{
 		try
 		{
@@ -228,11 +352,16 @@ public class BoardController
 		}
 		catch(ArrayIndexOutOfBoundsException e)
 		{
-			
+			e.printStackTrace();
 		}
 	}
 	
-	public void LookRight(int positionX, int positionY)
+	/**
+	 * Vérifie à droite du jeton à la position spécifiée.
+	 * @param positionX La position du jeton en X
+	 * @param positionY La position du jeton en Y
+	 */
+	private void LookRight(int positionX, int positionY)
 	{
 		try
 		{
@@ -244,32 +373,50 @@ public class BoardController
 		}
 		catch(ArrayIndexOutOfBoundsException e)
 		{
-			
+			e.printStackTrace();
 		}
 	}
 	
+	/**
+	 * Retourne le joueur actif
+	 * @return Le joueur actif
+	 */
 	public State GetCurrentPlayer()
 	{
 		return this.currentPlayer;
 	}
 	
+	/**
+	 * Retourne la vue Menu
+	 * @return La vue Menu
+	 */
 	public MenuView GetMenuView() 
 	{
 		return this.menuView;
 	}
 
+	/**
+	 * Retourne les settings de l'application
+	 * @return Un tableau de settings
+	 */
 	public int[] GetSettings()
 	{
 		return this.settings;
 	}
 	
+	/**
+	 * Permet de modifier les settings de l'application
+	 * @param settings Un tableau de nouveaux settings
+	 */
 	public void SetSettings(int[] settings)
 	{
 		this.settings = settings;
 	}
 
-	
-	
+	/**
+	 * Point de départ de l'application
+	 * @param args
+	 */
 	public static void main(String[] args)
 	{
 		@SuppressWarnings("unused")
