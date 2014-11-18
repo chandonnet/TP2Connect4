23ee5cc218ce677b6df9119c8f3cc8331f659aa1
GabLeg <moi_lo_lo_lol-14@hotmail.com>
2014-11-18 15:37:26 -0500

    java doc

--- src/model/PreviousPlaysModel.java
+++ src/model/PreviousPlaysModel.java
@@ -2,15 +2,27 @@ package model;
 
 import java.util.Stack;
 
+/**
+ * La classe PreviousPlaysModel représente une stack de jeu remplie des actions précédentes
+ * qui peuvent être remises à jeu.
+ * @author Gabriel
+ */
 public class PreviousPlaysModel 
 {
 	private Stack<PositionReverse> lastPlays;
 	
+	/**
+	 * Constructeur de PreviousPlaysModel
+	 */
 	public PreviousPlaysModel()
 	{
 		this.lastPlays = new Stack<PositionReverse>();
 	}
 	
+	/**
+	 * Fonction qui retourne la dernière action qui a été faite
+	 * @return lastPlays.pop() dernière action
+	 */
 	public PositionReverse ReversePlay()
 	{
 		if (this.lastPlays.size() == 1)
@@ -21,6 +33,10 @@ public class PreviousPlaysModel
 		return this.lastPlays.pop();
 	}
 	
+	/**
+	 * Fonction qui ajoute la dernière action dans la stack.
+	 * @param position  -La position de la nouvelle action
+	 */
 	public void AddPlay(PositionReverse position)
 	{
 		this.lastPlays.push(position);
